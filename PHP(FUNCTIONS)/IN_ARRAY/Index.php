c9dc37ebb2241c69bf730f2c8c2c502390ad99d0
Gourav629 <52229412+Gourav629@users.noreply.github.com>
2020-07-09 15:20:26 +0530

    Function in PHP
    Checking the element in the specific array

---
+++ PHP(FUNCTIONS)/IN_ARRAY/Index.php
@@ -0,0 +1,9 @@
+<?php
+$array = array('fox','bear','deer','dog','cat');
+if (in_array('fox', $array)){
+	echo 'The Data is found';
+}
+else{
+	echo "CANNOT LOOP THROUGH THE VARIABLE, NOT AN ARRAY";
+}
+?>
